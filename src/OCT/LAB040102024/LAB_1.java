d042be283fe2cdba9aaa9aca849b982eb03e8c7e
Manthan <manthanpanchal8055@gmail.com>
2024-10-06 18:07:03 +0530

    Java_Learning

--- src/OCT/LAB040102024/LAB_1.java
+++ src/OCT/LAB040102024/LAB_1.java
@@ -1,4 +1,29 @@
 package OCT.LAB040102024;
 
 public class LAB_1 {
+    public static void main(String[] args) {
+        System. out. print("10 x 1 =");
+        System. out. println(10*1);
+        System. out. print("10 x 2 =");
+        System. out. println(10*2);
+        System. out. print("10 x 3 =");
+        System. out. println(10*3);
+        System. out. print("10 x 4 =");
+        System. out. println(10*4);
+        System. out. print("10 x 5 =");
+        System. out. println(10*5);
+        System. out. print("10 x 6 =");
+        System. out. println(10*6);
+        System. out. print("10 x 7 =");
+        System. out. println(10*7);
+        System. out. print("10 x 8 =");
+        System. out. println(10*8);
+        System. out. print("10 x 9 =");
+        System. out. println(10*9);
+        System. out. print("10 x 10 =");
+        System. out. println(10*10);
+
+    }
+
 }
+
--- src/OCT/LAB040102024/LAB_2.java
+++ src/OCT/LAB040102024/LAB_2.java
@@ -1,4 +1,8 @@
 package OCT.LAB040102024;
 
 public class LAB_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+
 }
