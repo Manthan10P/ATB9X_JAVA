360ac4c5a17c6734fcc56db1787b555ae448c04b
Manthan <manthanpanchal8055@gmail.com>
2024-10-09 19:13:30 +0530

    Java_Learning

---
+++ src/OCT/LAB040102024/LAB_3.java
@@ -0,0 +1,20 @@
+package OCT.LAB040102024;
+
+public class LAB_3 {
+    public static void main(String[] args) {
+        int num_1 = 56;
+        int num_2 = 46;
+        int num_3;
+
+        num_3 = num_1 + num_2;
+        System.out.println(num_3);
+        num_3 = num_1 - num_2;
+        System.out.println(num_3);
+        num_3 = num_1 * num_2;
+        System.out.println(num_3);
+        num_3 = num_1 / num_2;
+        System.out.println(num_3);
+        num_3 = num_1 % num_2;
+        System.out.println(num_3);
+    }
+}
