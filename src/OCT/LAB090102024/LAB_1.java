43d07799293af75ef2b936372e28cf2524f50cf6
Manthan <manthanpanchal8055@gmail.com>
2024-10-09 19:07:31 +0530

    Java_Learning

---
+++ src/OCT/LAB090102024/LAB_1.java
@@ -0,0 +1,18 @@
+package OCT.LAB090102024;
+
+public class LAB_1 {
+    public static void main(String[] args) {
+        int NUM = 75;
+        System.out.printf("%d * %d = %d\n", NUM,1, NUM);
+        System.out.printf("%d * %d = %d\n", NUM,2, NUM * 2);
+        System.out.printf("%d * %d = %d\n", NUM,3, NUM * 3);
+        System.out.printf("%d * %d = %d\n", NUM,4, NUM * 4);
+        System.out.printf("%d * %d = %d\n", NUM,5, NUM * 5);
+        System.out.printf("%d * %d = %d\n", NUM,6, NUM * 6);
+        System.out.printf("%d * %d = %d\n", NUM,7, NUM * 7);
+        System.out.printf("%d * %d = %d\n", NUM,8, NUM * 8);
+        System.out.printf("%d * %d = %d\n", NUM,9, NUM * 9);
+        System.out.printf("%d * %d = %d\n", NUM,10, NUM * 10);
+
+    }
+}
